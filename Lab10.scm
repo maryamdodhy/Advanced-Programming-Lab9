fb48ed56d56cb2632803502526566fc69a1895fa
maryamdodhy <m.dodhy94@gmail.com>
2016-05-22 23:49:16 +0500

    Add files via upload

---
+++ Lab10.scm
@@ -0,0 +1,27 @@
+(define Add
+	(lambda (m n s z)
+		(m s (n s z))))
+
+(define Subtract
+	(lambda (m n)
+		(n pred m)))
+
+(define And
+	(lambda (m n a b) 
+		(n (m a b) b)))
+		
+(define Or
+	(lambda (m n a b) 
+		(n a (m a b))))
+
+(define Not
+	(lambda (m a b) 
+		(m a b)))
+
+(define LEQ
+	(lambda (m n) 
+		(isZero (Subtract m n))))
+		
+(define GEQ 
+	(lambda(m n)
+		(Or (EQ(m n) NOT(LEQ(n m))))))
